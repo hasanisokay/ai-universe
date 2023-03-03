9e2670c98ecf8231925038b135c1edd6e97facb4
hasanisokay <hasanisokay@gmail.com>
2023-03-03 10:20:44 +0600

    features and integrations added dynamically through looping

--- javascript/ai.js
+++ javascript/ai.js
@@ -60,51 +60,73 @@ const fetchModal = id =>{
 }
 
 const displayModal = data =>{
-    const {accuracy, description, features, image_link, input_output_examples, integrations, logo, pricing, tool_name}= data
+    const {accuracy, description, features, id, image_link, input_output_examples, integrations, logo, pricing, tool_name}= data || {}
     console.log(data)
     modalContainer.innerHTML=`
         <div style="background: rgba(235, 87, 87, 0.05); border: 1px solid #EB5757; border-radius: 16px;">
             <h4>${description}</h4>
             <div class="d-flex gap-4 justify-content-center">
                 <div class="bg-white rounded text-success fw-bold px-2 text-center d-flex align-items-center">
-                    <p>${pricing[0].price =="No cost" ? "Free of Cost" : pricing[0].price} <br> <span>Basic</span></p>
+                    <p>${pricing ? pricing[0].price ==="No cost" ? "Free of Cost" : pricing[0].price : "Free of Cost"} <br> <span>Basic</span></p>
                 </div>
                 <div class="bg-white rounded text-warning-emphasis fw-bold px-2 text-center d-flex align-items-center ">
-                    <p>${pricing[1].price =="No cost" ? "Free of Cost" : pricing[1].price} <br> <span>Pro</span></p>
+                    <p>${pricing ?  pricing[1].price ==="No cost" ? "Free of Cost" : pricing[1].price : "Free of Cost"} <br> <span>Pro</span></p>
                 </div>
                 <div class="bg-white rounded text-danger fw-bold px-2 text-center d-flex align-items-center">
-                    <p>${pricing[2].price =="No cost" ? "Free of Cost" : pricing[2].price} <br> <span>${pricing[2].plan}</span></p>
+                    <p>${pricing ? pricing[2].price : "Free of Cost"} <br> <span>${pricing ? pricing[2].plan : "Enterprise"}</span></p>
                 </div>
             </div>
             <div class="d-flex gap-4 ps-4">
                 <div>
                     <h4 class="fw-bold">Features</h4>
-                    <ul>
-                        <li>${features[1].feature_name}</li>
-                        <li>${features[2].feature_name}</li>
-                        <li>${features[3].feature_name}</li>
+                    <ul id="features-ul">
                     </ul>
                 </div>
                 <div>
                     <h4 class="fw-bold">Integrations</h4>
-                    <ul>
-                        <li>${integrations[0] ? integrations[0] :"No data found" }</li>
-                        <li>${integrations[1] ? integrations[1] :""}</li>
-                        <li>${integrations[2] ? integrations[2] :""}</li>
-                    </ul>
+                    <div id="integrations-div"> 
+                        <ul id="integrations-ul">
+                        </ul>
+                    </div>
+                    
                 </div>
             </div>
         </div>
         <div>
             <div><img src='${image_link[0]}' class="img-fluid"></div>
             <div class="text-center my-4 mx-1">
-                <h4 class="fw-bold">${input_output_examples[0].input ? input_output_examples[0].input : "Can you give any example?" }</h4>
-                <p class="text-body-secondary">${input_output_examples[0].output ? input_output_examples[0].output : "No! Not Yet! Take a break!!!"}</p>
+                <h4 class="fw-bold">${input_output_examples ? input_output_examples[0].input : "Can you give any example?" }</h4>
+                <p class="text-body-secondary">${input_output_examples ? input_output_examples[0].output : "No! Not Yet! Take a break!!!"}</p>
             </div>
         </div>
                 
     `
+    integrationsUl(integrations)
+    featuresUl(features)
 }
-
+// no data integrations error handler and
+// data integrations adder to list
+const integrationsUl =(integrations)=>{
+    const ul = document.getElementById("integrations-ul")    
+    if(!integrations){
+        document.getElementById("integrations-div").innerHTML = `
+        <p>No data found</p>
+        `
+        return
+    }
+    integrations.forEach(element =>{
+        ul.innerHTML +=`
+        <li>${element}</li>
+        `
+    })
+}
+// features items loop through and add to ul
+const featuresUl = features =>{
+    const ul = document.getElementById('features-ul')
+    for (element in features){
+        ul.innerHTML += `
+        <li>${features[element].feature_name}</li>`
+    }
+}
 
 
